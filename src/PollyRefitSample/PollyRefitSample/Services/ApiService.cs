63acd09489e16dc1d40454f419e3a39284329e99
Eduardo <eduardo@forlogic.net>
2020-07-17 13:29:35 -0300

    Integracao polly + refit completa

--- src/PollyRefitSample/PollyRefitSample/Services/ApiService.cs
+++ src/PollyRefitSample/PollyRefitSample/Services/ApiService.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using PollyRefitSample.Models;
 using Refit;
@@ -20,18 +19,21 @@ namespace PollyRefitSample.Services
     public class ApiService : IApiService
     {
         private readonly IPokemonFunctions _pokemonFunctions;
+        private readonly IPollyService _pollyService;
         
         public ApiService()
         {
-            _pokemonFunctions = RestService.For<IPokemonFunctions>("https://pokeapi.co/api/v2");
+            _pokemonFunctions = RestService.For<IPokemonFunctions>("https://pokeapi.co/api/v3");
+            _pollyService = new PollyService();
         }
-
 
         public async Task<Pokemon> GetPokemon(int id)
         {
             try
             {
-                return await _pokemonFunctions.GetPokemon(id);
+                var func = new Func<Task<Pokemon>>(() => _pokemonFunctions.GetPokemon(id));
+                
+                return await _pollyService.WaitAndRetry<Pokemon>(func, 10, 3);
             }
             catch (Exception e)
             {
--- src/PollyRefitSample/PollyRefitSample/Services/PollyService.cs
+++ src/PollyRefitSample/PollyRefitSample/Services/PollyService.cs
@@ -9,30 +9,30 @@ namespace PollyRefitSample.Services
         Task<T> Retry<T>(Func<Task<T>> func);
         Task<T> Retry<T>(Func<Task<T>> func, int retryCount);
         Task<T> Retry<T>(Func<Task<T>> func, int retryCount, Func<Exception, int, Task> onRetry);
-        Task<T> WaitAndRetry<T>(Func<Task<T>> func, Func<int, TimeSpan> sleepDurationProvider);
-        Task<T> WaitAndRetry<T>(Func<Task<T>> func, Func<int, TimeSpan> sleepDurationProvider, int retryCount);
-        Task<T> WaitAndRetry<T>(Func<Task<T>> func, Func<int, TimeSpan> sleepDurationProvider, int retryCount, Func<Exception, TimeSpan, Task> onRetryAsync);
+        Task<T> WaitAndRetry<T>(Func<Task<T>> func, int secondsWait);
+        Task<T> WaitAndRetry<T>(Func<Task<T>> func, int secondsWait, int retryCount);
+        Task<T> WaitAndRetry<T>(Func<Task<T>> func, int secondsWait, int retryCount, Func<Exception, TimeSpan, Task> onRetryAsync);
     }
 
     public class PollyService : IPollyService
     {
-        public async Task<T> Retry<T>(Func<Task<T>> func) => 
+        public async Task<T> Retry<T>(Func<Task<T>> func) =>
             await RetryInner(func);
 
-        public async Task<T> Retry<T>(Func<Task<T>> func, int retryCount) => 
+        public async Task<T> Retry<T>(Func<Task<T>> func, int retryCount) =>
             await RetryInner(func, retryCount);
 
-        public async Task<T> Retry<T>(Func<Task<T>> func, int retryCount, Func<Exception, int, Task> onRetry) => 
+        public async Task<T> Retry<T>(Func<Task<T>> func, int retryCount, Func<Exception, int, Task> onRetry) =>
             await RetryInner(func, retryCount, onRetry);
 
-        public async Task<T> WaitAndRetry<T>(Func<Task<T>> func, Func<int, TimeSpan> sleepDurationProvider) => 
-            await WaitAndRetryInner<T>(func, sleepDurationProvider);
+        public async Task<T> WaitAndRetry<T>(Func<Task<T>> func, int secondsWait) =>
+            await WaitAndRetryInner<T>(func, secondsWait);
 
-        public async Task<T> WaitAndRetry<T>(Func<Task<T>> func, Func<int, TimeSpan> sleepDurationProvider, int retryCount) =>
-            await WaitAndRetryInner<T>(func, sleepDurationProvider, retryCount);
+        public async Task<T> WaitAndRetry<T>(Func<Task<T>> func, int secondsWait, int retryCount) =>
+            await WaitAndRetryInner<T>(func, secondsWait, retryCount);
 
-        public async Task<T> WaitAndRetry<T>(Func<Task<T>> func, Func<int, TimeSpan> sleepDurationProvider, int retryCount, Func<Exception, TimeSpan, Task> onRetryAsync) =>
-            await WaitAndRetryInner<T>(func, sleepDurationProvider, retryCount, onRetryAsync);
+        public async Task<T> WaitAndRetry<T>(Func<Task<T>> func, int secondsWait, int retryCount, Func<Exception, TimeSpan, Task> onRetryAsync) =>
+            await WaitAndRetryInner<T>(func, secondsWait, retryCount, onRetryAsync);
 
         #region Inner Methods
 
@@ -42,7 +42,8 @@ namespace PollyRefitSample.Services
             {
                 return Task.Factory.StartNew(() =>
                 {
-                    Console.WriteLine($"Retry #{i} due to exception '{(e.InnerException ?? e).Message}'"); //TODO Futura integracao com o AppCenter
+                    Console.WriteLine(
+                        $"Retry #{i} in {DateTimeOffset.Now} due to exception '{(e.InnerException ?? e).Message}'"); //TODO Futura integracao com o AppCenter
                 });
             });
 
@@ -51,15 +52,18 @@ namespace PollyRefitSample.Services
                 .ExecuteAsync<T>(func);
         }
 
-        private async Task<T> WaitAndRetryInner<T>(Func<Task<T>> func, Func<int, TimeSpan> sleepDurationProvider, int retryCount = 1, Func<Exception, TimeSpan, Task> onRetryAsync = null)
+        private async Task<T> WaitAndRetryInner<T>(Func<Task<T>> func, int secondsWait, int retryCount = 1, Func<Exception, TimeSpan, Task> onRetryAsync = null)
         {
             var onRetryInner = new Func<Exception, TimeSpan, Task>((e, t) =>
             {
                 return Task.Factory.StartNew(() =>
                 {
-                    Console.WriteLine($"Retrying in {t.ToString("g")} due to exception '{(e.InnerException ?? e).Message}'"); //TODO Futura integracao com o appCenter
+                    Console.WriteLine(
+                        $"Retrying in {t.ToString("g")} due to exception '{(e.InnerException ?? e).Message}' in {DateTimeOffset.Now} "); //TODO Futura integracao com o appCenter
                 });
-            });
+            }); ;
+                
+            var sleepDurationProvider = new Func<int, TimeSpan>((i => TimeSpan.FromSeconds(secondsWait)));
 
             return await Policy.Handle<Exception>()
                 .WaitAndRetryAsync(retryCount, sleepDurationProvider, onRetryAsync ?? onRetryInner)
